d9e58b9389735b954f11c59bf69a72232f735bc8
Guilherme Lopes <guilherme.lopes3@academico.ufpb.br>
2026-03-08 19:24:26 -0300

    fix: typo in about sect

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -23,7 +23,7 @@ export default function About() {
             {[
               `cs student at federal university of paraíba (ufpb) and member of tail — the technology and artificial intelligence league. that's where most of the interesting problems come from.`,
               `at aria laboratory, i design data and backend infrastructure — schemas, microservices, pipelines, api's - you get the idea. at moises.ai, i built an audio analysis api for chord notes recognition.`,
-              `i also did ml research with the university of pennsylvania, helping test and rate tissue separation with machine learning on medical imaging classification. `
+              `i also did ml research with the university of pennsylvania, helping test and rate tissue separation with machine learning on medical imaging classification.`,
               `through tail's music board, i'm also part of a joint project developing ai-driven haptic systems to make music accessible to hearing-impaired people.`,
               `i am very much obsessed with anything world-changing, especially if it envolves music, ai, data and, well, code.`,
             ].map((text, i) => (
